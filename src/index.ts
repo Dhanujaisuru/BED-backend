0259a1537d4971676003bc3751e8e71aad690b30
Dhanuja Isuru <128521002+Dhanujaisuru@users.noreply.github.com>
2025-02-08 10:24:25 +0530

    week-16-code

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,12 @@ import { categoryRouter } from './api/category';
 import { connectDB } from './infrastructure/db';
 import globalErrorHandlingMiddleware from './api/middleware/global-error-handling-middleware';
 import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import { orderRouter } from "./api/order";
+import { paymentsRouter } from "./api/payment";
 
 app.use(express.json()); // For parsing JSON requests
+app.use(clerkMiddleware());
 app.use(cors({ origin: "http://localhost:5173" }));
 
 // app.use((req, res, next) => {
@@ -19,8 +23,10 @@ app.use(cors({ origin: "http://localhost:5173" }));
 //     console.log(req.method, req.url);
 // })
 
-app.use('/api/products', productRouter);
-app.use('/api/categories', categoryRouter);
+app.use("/api/products", productRouter);
+app.use("/api/categories", categoryRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/payments", paymentsRouter);
 app.use(globalErrorHandlingMiddleware);
 
 connectDB();
